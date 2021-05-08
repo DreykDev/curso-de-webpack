7bf92398fc5898fc3b8d0479c8e315352e93d814
David Valencia <codingdavid9@outlook.com>
2021-05-07 20:04:04 -0500

    Netlify

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { copyFile } = require('fs')
 
 module.exports = {
